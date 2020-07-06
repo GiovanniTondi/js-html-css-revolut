54c5ba7cd8bc639e2a9ee2f9a1ef61c48afc5d37
Giovanni <tondigiovanni@gmail.com>
2020-07-06 20:26:10 +0200

    Completato

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 
 $(document).ready(function () {
+
+    // Clona il menu
+    $('.parent-dropdown').clone().appendTo('.menu-xs');
+
+
     $('.parent-dropdown').mouseenter(function () {
         if (!$('.menu-xs').hasClass('active')) {
             $(this).children('div').show();
@@ -14,18 +19,31 @@ $(document).ready(function () {
 
     $('.parent-dropdown').click(function () {
         if ($('.menu-xs').hasClass('active')) {
-            $(this).children('div').toggle();
+            $('.parent-dropdown').children('div').slideUp();
+
+            if ($(this).hasClass('active')) {
+                $('.parent-dropdown').removeClass('active');
+                $(this).children('div').slideUp();
+            } else {
+                $('.parent-dropdown').removeClass('active');
+                $(this).addClass('active')
+                $(this).children('div').slideDown();
+            }
+
         }
     });
 
 
     $('.hamburger').click(function () {
         $('.hamburger').toggleClass('hidden-xs');
-
-
+        $('main').hide();
+        
+        // Apparire
         $('.menu-xs').toggleClass('active');
         $('header').toggleClass('hidden-xs');
         $('header .inline-menu.left').toggleClass('hidden-xs');
+
+        $('.parent-dropdown').toggleClass('hidden-xs');
     });
 
     $('.close').click(function () {
@@ -33,6 +51,9 @@ $(document).ready(function () {
         $('header').toggleClass('hidden-xs');
         $('header .inline-menu.left').toggleClass('hidden-xs');
 
+        // Apparire
+        $('main').show();
+        $('.parent-dropdown').toggleClass('hidden-xs');
 
         $('.hamburger').toggleClass('hidden-xs');
     });
