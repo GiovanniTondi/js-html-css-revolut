f4aed28d191e526fcf65a67c0cbbe4124c67c7ca
Giovanni <tondigiovanni@gmail.com>
2020-07-06 19:10:47 +0200

    js hamburger click close

--- js/script.js
+++ js/script.js
@@ -1,11 +1,40 @@
 
 $(document).ready(function () {
     $('.parent-dropdown').mouseenter(function () {
-        $(this).children('div').show();
+        if (!$('.menu-xs').hasClass('active')) {
+            $(this).children('div').show();
+        }
     });
 
     $('.parent-dropdown').mouseleave(function () {
-        $(this).children('div').hide();
+        if (!$('.menu-xs').hasClass('active')) {
+            $(this).children('div').hide();
+        }
+    });
+
+    $('.parent-dropdown').click(function () {
+        if ($('.menu-xs').hasClass('active')) {
+            $(this).children('div').toggle();
+        }
+    });
+
+
+    $('.hamburger').click(function () {
+        $('.hamburger').toggleClass('hidden-xs');
+
+
+        $('.menu-xs').toggleClass('active');
+        $('header').toggleClass('hidden-xs');
+        $('header .inline-menu.left').toggleClass('hidden-xs');
+    });
+
+    $('.close').click(function () {
+        $('.menu-xs').toggleClass('active');
+        $('header').toggleClass('hidden-xs');
+        $('header .inline-menu.left').toggleClass('hidden-xs');
+
+
+        $('.hamburger').toggleClass('hidden-xs');
     });
 
 });
