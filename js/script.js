d65016d064b5e71177f30df5422e68074176c683
Giovanni <tondigiovanni@gmail.com>
2020-07-07 13:23:25 +0200

    Fix js + aggiunta arrow dropdown hamburger

--- js/script.js
+++ js/script.js
@@ -2,20 +2,23 @@
 $(document).ready(function () {
 
     $('.parent-dropdown').mouseenter(function () {
-        if (!$('.inline-menu').hasClass('active')) {
+        if ($('.inline-menu.active').css('flex-direction') != 'column') {
             $(this).children('div').show();
         }
     });
 
     $('.parent-dropdown').mouseleave(function () {
-        if (!$('.inline-menu').hasClass('active')) {
+        if ($('.inline-menu.active').css('flex-direction') != 'column') {
             $(this).children('div').hide();
+            $('.parent-dropdown').removeClass('active');
         }
     });
 
     $('.parent-dropdown').click(function () {
-        if ($('.inline-menu').hasClass('active')) {
+
+        if ($('.inline-menu.active').css('flex-direction') == 'column') {
             $('.parent-dropdown').children('div').slideUp();
+            $('.parent-dropdown .fas').removeClass('fa-chevron-up').addClass('fa-chevron-down');
 
             if ($(this).hasClass('active')) {
                 $('.parent-dropdown').removeClass('active');
@@ -24,8 +27,8 @@ $(document).ready(function () {
                 $('.parent-dropdown').removeClass('active');
                 $(this).addClass('active')
                 $(this).children('div').slideDown();
+                $('.parent-dropdown.active .fas').toggleClass('fa-chevron-down fa-chevron-up');
             }
-
         }
     });
 
